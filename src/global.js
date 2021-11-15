22d59393823fd3909d66e2740746e1101677018c
ahmed <ahmedlearn01@gmail.com>
2021-11-15 11:55:22 -0800

    initial commit

--- src/global.js
+++ src/global.js
@@ -1,21 +1,21 @@
-window.addEventListener('DOMContentLoaded',()=>{
+// window.addEventListener('DOMContentLoaded',()=>{
 
-    const navContent = document.querySelector('.nav-content');
+//     const navContent = document.querySelector('.nav-content');
 
-    navContent.addEventListener('click',(e)=>{
-        if(e.target.classList.contains('bars')){
-            const bars = document.querySelector('.bars');
-            const tabs = document.querySelector('.tabs');
-            const close = document.querySelector('.close');
+//     navContent.addEventListener('click',(e)=>{
+//         if(e.target.classList.contains('bars')){
+//             const bars = document.querySelector('.bars');
+//             const tabs = document.querySelector('.tabs');
+//             const close = document.querySelector('.close');
 
-            bars.addEventListener('click',()=>{
-                tabs.classList.remove('invisible');
-            });
+//             bars.addEventListener('click',()=>{
+//                 tabs.classList.remove('invisible');
+//             });
 
-            close.addEventListener('click',()=>{
-                tabs.classList.add('invisible')
-            })
-        }
-    })
+//             close.addEventListener('click',()=>{
+//                 tabs.classList.add('invisible')
+//             })
+//         }
+//     })
 
-})
+// })
