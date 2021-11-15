b69c7e1798c979b3f764f304c153ecdb75dde324
ahmed <ahmedlearn01@gmail.com>
2021-11-15 09:25:24 -0800

    responsive

--- src/global.js
+++ src/global.js
@@ -1,1 +1,17 @@
+const navContent = document.querySelector('.nav-content');
 
+navContent.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('bars')){
+        const bars = document.querySelector('.bars');
+        const tabs = document.querySelector('.tabs');
+        const close = document.querySelector('.close');
+
+        bars.addEventListener('click',()=>{
+            tabs.classList.remove('invisible');
+        });
+
+        close.addEventListener('click',()=>{
+            tabs.classList.add('invisible')
+        })
+    }
+})
