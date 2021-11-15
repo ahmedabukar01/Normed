94e9c315197b6f936d0c42642706a80c0d328ed2
ahmed <ahmedlearn01@gmail.com>
2021-11-15 09:15:48 -0800

    res

--- src/pages/home/components/Navbar.js
+++ src/pages/home/components/Navbar.js
@@ -1,11 +1,18 @@
+import {FaBars} from 'react-icons/fa';
+import {FaWindowClose} from 'react-icons/fa';
+
 const Navbar = () =>{
+   
     return (
         <div className="navbar">
             <div className="nav-content">
                 <div className="logo">
                     <span>NT</span>
                 </div>
-                <ul className="tabs">
+                <div className="tabs">
+                    <FaWindowClose className="close" />
+                <ul>
+                    
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Services</a></li>
@@ -13,6 +20,9 @@ const Navbar = () =>{
                     <li><a href="#">Contact</a></li>
                     <li><a href="#">Resources</a></li>
                 </ul>
+                </div>
+                <FaBars className="bars visible" />
+                
             </div>
         </div>
     )
