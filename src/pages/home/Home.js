8ba34a06b94f0cb53ef58deb7b275a7d6840c6d8
ahmed <ahmedlearn01@gmail.com>
2021-11-15 19:06:50 -0800

    initial commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,23 @@
 import Navbar from './components/Navbar';
+import bgImage from '../images/banner1.jpg';
 
 const Home = () =>{
+      const styles = {
+          backgroundImage: `radial-gradient(rgba(7, 5, 5, 0.808),rgba(39, 34, 34, 0.74)), URL(${bgImage})`
+      }
     return (
-        <div className="home">
+        <div className="home" style={styles}>
             <Navbar />
         </div>
     )
 }
 export default Home;
+
+// .home{
+//     background:
+//     height:100vh;
+//     background-position:center;
+//     background-size: cover;
+//     overflow-x: hidden;
+//     position: relative;
+//   }
