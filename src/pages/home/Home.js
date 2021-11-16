2cdbe3f9c8bd31c64aeb20a445b20e79e3f7ba8d
ahmed <ahmedlearn01@gmail.com>
2021-11-15 19:11:48 -0800

    initial commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,12 @@ import bgImage from '../images/banner1.jpg';
 
 const Home = () =>{
       const styles = {
-          backgroundImage: `radial-gradient(rgba(7, 5, 5, 0.808),rgba(39, 34, 34, 0.74)), URL(${bgImage})`
+          backgroundImage: `radial-gradient(rgba(7, 5, 5, 0.808),rgba(39, 34, 34, 0.74)), URL(${bgImage})`,
+          height: '100vh',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          overflowX: 'hidden',
+          position: 'relative'
       }
     return (
         <div className="home" style={styles}>
