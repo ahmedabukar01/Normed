6c1b57f0b85122b1cd4c061e699d0bd5b520b165
ahmed <ahmedlearn01@gmail.com>
2021-11-15 19:23:58 -0800

    initial commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,8 @@
 import Navbar from './components/Navbar';
-import bgImage from '../images/banner1.jpg';
+import bgImage from '../images/banner2.jpg';
 
 const Home = () =>{
-      const styles = {
-          backgroundImage: `radial-gradient(rgba(7, 5, 5, 0.808),rgba(39, 34, 34, 0.74)), URL(${bgImage})`,
-          height: '100vh',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          overflowX: 'hidden',
-          position: 'relative'
-      }
+    
     return (
         <div className="home" style={styles}>
             <Navbar />
@@ -18,11 +11,12 @@ const Home = () =>{
 }
 export default Home;
 
-// .home{
-//     background:
-//     height:100vh;
-//     background-position:center;
-//     background-size: cover;
-//     overflow-x: hidden;
-//     position: relative;
-//   }
+// css
+const styles = {
+    backgroundImage: `radial-gradient(rgba(7, 5, 5, 0.808),rgba(39, 34, 34, 0.74)), URL(${bgImage})`,
+    height: '100vh',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    overflowX: 'hidden',
+    position: 'relative'
+}
---
+++ src/pages/home/components/Showcase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Showcase = () => {
+    return (
+        <div className="showcase">
+            
+        </div>
+    )
+}
+
+export default Showcase;
