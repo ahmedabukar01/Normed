b85abb8f09882e6df84c78358dead1720e6ee58d
ahmed <ahmedlearn01@gmail.com>
2021-11-17 08:26:01 -0800

    initial commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-import bgImage from '../images/banner2.jpg';
+import bgImage from '../images/banner3.jpg';
 import Showcase from './components/Showcase';
 
 const Home = () =>{
