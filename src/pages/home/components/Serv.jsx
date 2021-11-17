b73c5f0f7c61e167a7cb2c8d9bad27ea66e27c51
ahmed <ahmedlearn01@gmail.com>
2021-11-17 11:51:39 -0800

    working well as unknown

--- src/pages/home/components/Serv.jsx
+++ src/pages/home/components/Serv.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { FaDesktop } from "react-icons/fa"
+import { FaCode } from "react-icons/fa"
 
 const Serv = () => {
     return (
         <div className="service-content">
-            content in progress.....
+            <div className="serv-box">
+                <FaCode  className="icon"/>
+                <h3>Web Development</h3>
+                <p>Lorem ipsum dolor, sit amet 
+                consectetur adipisicing elit. Ducimus, deserunt?</p>
+            </div>
+
+            <div className="serv-box sp">
+                <FaDesktop className="icon"/>
+                <h3>Graphic Design</h3>
+                <p>Lorem ipsum dolor, sit amet 
+                consectetur adipisicing elit. Ducimus, deserunt?</p>
+            </div>
+
+            <div className="serv-box">
+                <FaCode className="icon"/>
+                <h3>Support</h3>
+                <p>Lorem ipsum dolor, sit amet 
+                consectetur adipisicing elit. Ducimus, deserunt?</p>
+            </div>
         </div>
     )
 }
