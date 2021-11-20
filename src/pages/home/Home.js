1da9e4bef313cb53ac7d9cb03227d2ceaff09200
ahmed <ahmedlearn01@gmail.com>
2021-11-20 12:28:05 -0800

    unkown

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,7 +15,7 @@ export default Home;
 
 // css
 const styles = {
-    backgroundImage: `radial-gradient(rgba(7, 5, 5, 0.858),rgba(39, 34, 34, 0.804)), URL(${bgImage})`,
+    backgroundImage: `radial-gradient(rgba(17, 15, 15, 0.858),rgba(19, 17, 18, 0.804)), URL(${bgImage})`,
     height: '100vh',
     backgroundPosition: 'center',
     backgroundSize: 'cover',
