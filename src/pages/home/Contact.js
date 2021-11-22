c728c68d0270e50f222a58569f1fd5f47d9b8372
ahmed <ahmedlearn01@gmail.com>
2021-11-22 09:43:58 -0800

    gogle maps

--- src/pages/home/Contact.js
+++ src/pages/home/Contact.js
@@ -4,12 +4,17 @@ import { FaPhone } from 'react-icons/fa';
 
 const Contact = () => {
     return (
-        <div className="contact">
+        <div className="contact center">
+            <h1>About Us</h1>
             <div className="contact-content">
                 <div className="contact-left">
+                    <div className="cont-text">
+                    <h4>Get in Touch </h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ipsam. Lorem ipsum dolor sit amet.</p>
+                    </div>
                     <div className="cont-box">
-                        <div className="icon">
-                            <FaMapMarkerAlt />
+                        <div className="icon-box">
+                            <FaMapMarkerAlt className="icon"/>
                         </div>
                         <div className="info">
                             <div className="title">
@@ -21,8 +26,8 @@ const Contact = () => {
                         </div>
                     </div>
                     <div className="cont-box">
-                        <div className="icon">
-                            <FaEnvelope />
+                        <div className="icon-box">
+                            <FaEnvelope className="icon"/>
                         </div>
                         <div className="info">
                             <div className="title">
@@ -34,21 +39,23 @@ const Contact = () => {
                         </div>
                     </div>
                     <div className="cont-box">
-                        <div className="icon">
-                            <FaPhone />
+                        <div className="icon-box">
+                            <FaPhone className="icon"/>
                         </div>
                         <div className="info">
                             <div className="title">
                                 Call us
                             </div>
                             <div className="subtitle">
-                                Taleeh, Hodan Distict, Banaadir
+                                +252 616 119 634
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="cont-right">
-
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31898.251514613832!2d45.30776470120546!3d2.0433094046665197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3d58414977b71633%3A0xe88dc3726b63cb60!2sNormed%20Technology!5e0!3m2!1sen!2sso!4v1637601515208!5m2!1sen!2sso"
+                 width="600" height="450" allowfullscreen="" loading="lazy"
+                 ></iframe>
                 </div>
             </div>
         </div>
